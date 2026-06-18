b38cc5c7c2f5eb34c6995375db3fa95ef57433cb
ysfsyln22 <ysfsyln22@gmail.com>
2026-06-18 12:20:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PROJE17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Satır Sayısını Giriniz : ");
+                int Sayi = Convert.ToInt32(Console.ReadLine());
+
+                string Sonuc = string.Empty;
+
+                for (int i = 0; i < Sayi; i++)
+                {
+                    Sonuc += "*";
+                    Console.WriteLine(Sonuc);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }   
+}
